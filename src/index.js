f66e8a8a0c32c16cf3a34e01fa506402fb4ae8a8
LarsM <lam@cphbusiness.dk>
2017-10-18 19:16:47 +0200

    Changed to use HashRouter

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
-import { HashRouter} from "react-router-dom"
+import { HashRouter,BrowserRouter} from "react-router-dom"
 import App from "./pages/App"
 
 
